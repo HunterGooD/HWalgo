400815bf3ad17059b0048d7fc242329e3c3b255f
Vlad Gudov <vlad.gudov@inbox.ru>
2022-01-07 02:04:42 +0400

    impl list data structure not finally

---
+++ c/list.h
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node Node;
+struct Node{
+    int n;
+    Node* next;
+};
+
+Node* newNode(int n) {
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    tmp->n = n;
+    return tmp;
+}
+
+typedef struct {
+    Node* head;
+    int length;
+}List;
+
+void addToListNode(List* l, int n); // Вставка в конец
+void addNode(Node* current, Node* add); // private
+void addNodeIdx(Node* node, Node* add,int currentIdx, int idx); //  private
+void addToListNodeIdx(List* l,int n, int idx); // Вставка по индексу
+void addToListNodeBeforeIdx(List* l,int n, int idx); // Вставка перед индексом
+void printNodes(Node* node, int idx);
+
+List* newList() {
+    List* l = (List*)malloc(sizeof(List));
+    l->head = NULL;
+    l->length = 0;
+    return l;
+}
+
+List* newListFromArray(int arr[], int length) {
+    List* l = (List*)malloc(sizeof(List));
+    l->head = NULL;
+    for (int i = 0; i < length; i++) {
+        addToListNode(l, arr[i]);
+    }
+    return l;
+}
+
+void addToListNode(List* l, int n){
+    Node *node = newNode(n);
+    node->next = NULL;
+    if (l->head == NULL) {
+        l->head = malloc(sizeof(Node));
+        l->head = node;
+    } else {
+        addNode(l->head, node);
+    }
+    l->length++;
+}
+
+void addToListNodeIdx(List* l,int n, int idx) {
+    if (idx > l->length) {
+        return;
+    }
+    Node *node = newNode(n);
+    if (idx == 1) {
+        node->next = l->head;
+        l->head = node;
+        return;
+    }
+    addNodeIdx(l->head, node, 0, idx);
+    l->length++;
+}
+
+void addNodeIdx(Node* node, Node* add, int currentIdx, int idx) {
+    if (currentIdx+1 == idx) {
+        add->next = node->next;
+        node->next = add;
+        return;
+    }
+    addNodeIdx(node->next, add, currentIdx+1, idx);
+}
+
+void addToListNodeBeforeIdx(List* l,int n, int idx) {
+
+}
+
+void addNode(Node* current, Node* add) {
+    if (current->next == NULL) {
+        current->next = malloc(sizeof(Node));
+        current->next = add;
+        return;
+    }
+    addNode(current->next, add);
+}
+
+void printList(List* l) {
+    if (l->head == NULL) {
+        printf("Length 0");
+        return;
+    }
+    printf("Length: %d Head: %d Tail: ", l->length, l->head->n);
+    printNodes(l->head->next, 1);
+    printf("> NULL\n");
+}
+
+void printNodes(Node* node, int idx) {
+    if (node->next == NULL) {
+        return;
+    }
+    printf("[%d] %d - ", idx, node->n);
+    printNodes(node->next, idx+1);
+}
--- c/main.c
+++ c/main.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
-#include "stack.h"
+#include <malloc.h>
+//#include "stack.h"
+#include "list.h"
 
 int main() {
-    stack *s = newStack();
-    push(s, 2);
-    push(s, 3);
-    push(s, 4);
-    push(s, 5);
-    stack *sCopy = copy(s);
-
-    printf("%d\n", *pop(s));
-    printf("%d\n", *pop(s));
-    printf("%d\n", *pop(s));
-    printf("%d\n", *pop(s));
-    int* a = pop(s);
-    if (a != NULL) {
-        printf("%d\n", *a);
+    /**
+     * Для просмотра работы структуры stack
+     */
+//    stack *s = newStack();
+//    push(s, 2);
+//    push(s, 3);
+//    push(s, 4);
+//    push(s, 5);
+//    stack *sCopy = copy(s);
+//
+//    printf("%d\n", *pop(s));
+//    printf("%d\n", *pop(s));
+//    printf("%d\n", *pop(s));
+//    printf("%d\n", *pop(s));
+//    int* a = pop(s);
+//    if (a != NULL) {
+//        printf("%d\n", *a);
+//    }
+//    printf("%d\n", *pop(sCopy));
+//    printf("%d\n", *pop(sCopy));
+//
+//    freeStack(&s);
+//    freeStack(&sCopy);
+    /**
+     * Для просмотра структуры List
+     */
+    int* arr = malloc(sizeof(int) * 10);
+    for (int i = 0; i < 10; i++) {
+        arr[i] = i+1;
     }
-    printf("%d\n", *pop(sCopy));
-    printf("%d\n", *pop(sCopy));
-
-    freeStack(&s);
-    freeStack(&sCopy);
+    List* l = newListFromArray(arr, 10);
+    addToListNode(l, 10);
+    addToListNodeIdx(l, 4, 3);
+    printList(l);
     return 0;
 }
